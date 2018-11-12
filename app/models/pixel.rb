3e80b78df6d1d62da54cc7b19d8024012b42591b
antprt <antoniopr.apr@gmail.com>
2018-11-12 23:21:29 +0100

    Pixel name is convert without spaces

--- app/models/pixel.rb
+++ app/models/pixel.rb
@@ -2,4 +2,10 @@ class Pixel < ApplicationRecord
   belongs_to :provider
   validates :provider, presence: true
   validates :name, presence: true, uniqueness: true, length: { minimum: 1, maximum: 100 }
+
+  before_validation :convert_name_without_space
+
+  def convert_name_without_space
+    self.name = self.name.gsub(" ","")
+  end
 end
